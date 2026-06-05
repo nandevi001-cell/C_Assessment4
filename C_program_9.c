c45ad3680cc8da9158b199f6e0c575fe606868b3
nandevi001-cell <nandevi001@gmail.com>
2026-06-05 22:29:55 +0530

    Remove unnecessary newline at end of C_program_9.c

---
+++ C_program_9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum,tens,ones;
+	i=12;
+	sum=0;
+	loop:if(i<=99)
+	{
+		tens=i/10;
+		ones=i%10;
+		if((tens+ones==6)&&(i%2==0))
+		{
+			printf("%d\n",i);
+		}
+		i=i+2;
+		goto loop;
+	}
+}
